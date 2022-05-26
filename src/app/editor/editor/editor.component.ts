8b976319d0a60c778254958ee37502449f99afc7
Pierre Tomasina <pierozi@pm.me>
2022-05-26 10:40:41 +0200

    feat(editor) svg

--- src/app/editor/editor/editor.component.ts
+++ src/app/editor/editor/editor.component.ts
@@ -13,6 +13,8 @@ export class EditorComponent implements AfterViewInit {
   public pageWidth: number = 424
 
   private elements = []
+  private app!: PIXI.Application
+  private editorContainer!: PIXI.Container
 
   constructor(public elRef: ElementRef, private zone: NgZone) {
   }
@@ -20,52 +22,84 @@ export class EditorComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.zone.runOutsideAngular(
       (): void => {
-        const pixiApp = new PIXI.Application({
+        this.app = new PIXI.Application({
           backgroundColor: 0xEEEEEE,
           width: this.pageWidth,
           height: this.pageHeight,
           view: this.canvasEditor.nativeElement,
         });
 
-        const container = new PIXI.Container()
-        pixiApp.stage.addChild(container)
+        this.editorContainer = new PIXI.Container()
+        this.app.stage.addChild(this.editorContainer)
 
-        const el = PIXI.Sprite.from('assets/plab-logo-opti.png')
-        el.anchor.set(0.5)
-        el.x = pixiApp.screen.width / 2
-        el.y = pixiApp.screen.height / 2
-        el.scale.set(.2)
-        el.interactive = true
-        el.buttonMode = true
-        el.on('pointerdown', this.onDragStart)
-          .on('pointerup', this.onDragEnd)
-          .on('pointerupoutside', this.onDragEnd)
-          .on('pointermove', this.onDragMove)
-        container.addChild(el)
-
-        const style = new PIXI.TextStyle({
-          fontFamily: 'Arial',
-          fontSize: 36,
-          fontStyle: 'italic',
-          fontWeight: 'bold',
-          fill: ['#ffffff', '#00ff99'], // gradient
-          stroke: '#4a1850',
-          strokeThickness: 5,
-          dropShadow: true,
-          dropShadowColor: '#000000',
-          dropShadowBlur: 4,
-          dropShadowAngle: Math.PI / 6,
-          dropShadowDistance: 6,
-          wordWrap: true,
-          wordWrapWidth: 440,
-          lineJoin: 'round',
-        });
-        const txt = new PIXI.Text("Foo bar baz", style)
-        txt.x = 50
-        txt.y = 100
-        container.addChild(txt)
+        this.app.renderer.render(this.app.stage)
       }
     )
+  }
+
+  cleanSlate() {
+    this.editorContainer.destroy()
+    this.editorContainer = new PIXI.Container()
+    this.app.stage.addChild(this.editorContainer)
+  }
+
+  async loadTemplate() {
+    // const el = PIXI.Sprite.from('assets/plab-logo-opti.png')
+    // el.anchor.set(0.5)
+    // el.x = this.app.screen.width / 2
+    // el.y = this.app.screen.height / 2
+    // el.scale.set(.2)
+    // el.interactive = true
+    // el.buttonMode = true
+    // el.on('pointerdown', this.onDragStart)
+    //   .on('pointerup', this.onDragEnd)
+    //   .on('pointerupoutside', this.onDragEnd)
+    //   .on('pointermove', this.onDragMove)
+    // this.editorContainer.addChild(el)
+
+    // TODO when click
+    // Add selection resizing square
+    // Add toolbox bar
+
+    this.app
+      .loader
+      .add("svgTest", "assets/plab-logo.svg")
+      .load((loader, res) => {
+        const svg = new PIXI.Sprite(res['svgTest'].texture)
+        svg.anchor.set(0.5)
+        svg.x = this.app.screen.width / 2
+        svg.y = this.app.screen.height / 2
+        svg.scale.set(.5)
+        svg.interactive = true
+        svg.buttonMode = true
+        svg.on('pointerdown', this.onDragStart)
+           .on('pointerup', this.onDragEnd)
+           .on('pointerupoutside', this.onDragEnd)
+           .on('pointermove', this.onDragMove)
+        this.editorContainer.addChild(svg)
+      })
+
+    const style = new PIXI.TextStyle({
+      fontFamily: 'Arial',
+      fontSize: 36,
+      fontStyle: 'italic',
+      fontWeight: 'bold',
+      fill: ['#ffffff', '#00ff99'], // gradient
+      stroke: '#4a1850',
+      strokeThickness: 5,
+      dropShadow: true,
+      dropShadowColor: '#000000',
+      dropShadowBlur: 4,
+      dropShadowAngle: Math.PI / 6,
+      dropShadowDistance: 6,
+      wordWrap: true,
+      wordWrapWidth: 440,
+      lineJoin: 'round',
+    });
+    const txt = new PIXI.Text("Foo bar baz", style)
+    txt.x = 50
+    txt.y = 100
+    this.editorContainer.addChild(txt)
   }
 
   onDragStart(event: any) {
